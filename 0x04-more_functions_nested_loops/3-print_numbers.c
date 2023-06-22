adfdda58bedb4e0710410f0bf48058ba034b0bd3
Elelwani <tshikosiem@gmail.com>
2023-06-22 14:52:58 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - print the number from 0 to 9
- * Return: the numbers from 0 to 9
+ * print_numbers - print the number from 0 up to 9
+ * Return: the numbers from 0 up to 9
  */
 void print_numbers(void)
 {
