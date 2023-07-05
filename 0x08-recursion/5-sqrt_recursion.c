1ea01eca383763800f2bc58672fffd4ea2327cdf
Elelwani <tshikosiem@gmail.com>
2023-07-05 16:06:24 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,29 +8,37 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (c % (n / c) == 0)
-	{
-		if (c * (n / c) == n)
-			return (c);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, c + 1));
-}
-
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: input number.
- * Return: natural square root.
- */
-
-int _sqrt_recursion(int n)
-{
 	if (n < 0)
+	{
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (power_operation(n, 2));
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	int start = 1;
+	int end = n;
+	int result = -1;
+
+	while (start <= end)
+
+	{
+		int mid = (start + end) / 2;
+
+		if (mid * mid == n)
+
+		{
+			return (mid);
+		}
+		else if (mid * mid < n)
+		{
+			start = mid + 1;
+			result = mid;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+	return (result);
 }
