d607ea8bac243554ebceee94d107bb96351a541d
Elelwani <tshikosiem@gmail.com>
2023-07-18 21:48:13 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - dog struct
- * @name: name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- * Description: dog struct decalaration
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
  */
-
-
 struct dog
 {
 	char *name;
@@ -17,12 +16,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t -Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
 
-#endif
+#endif /* struct dog */
